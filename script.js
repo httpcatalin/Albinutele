275d2380ecd311e4c5ef3c0b055f4f9c1f774ee3
Catalin <bitca.catalin@gmail.com>
2024-02-25 18:15:51 +0200

    functionalul navbarului

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  var menu = document.querySelector(".menu");
+  if (menu) menu.classList.toggle("visible");
+}
+function toggleBurger() {
+  var menu = document.querySelector(".menu-burger");
+  menu.classList.toggle("active");
+}
