8aef6615dcba15846d660d65f3e113eeca8e2bda
Chris-zin <crist5002@gmail.com>
2024-03-04 19:07:30 +0200

    Eliminarea necesitatilor si adaptarea la ceea ce este esential

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-  var menu = document.querySelector(".menu");
-  if (menu) menu.classList.toggle("visible");
-}
-function toggleBurger() {
-  var menu = document.querySelector(".menu-burger");
-  menu.classList.toggle("active");
-}
